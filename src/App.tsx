ceed4edfd29619190d4901ba8eb1fb9cc15fd002
machan <junmachan3962.dev@gmail.com>
2026-05-04 14:29:54 +0900

    ロジック部分はuseTodos.tsへ移動

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,15 @@
-import { useState, useEffect } from "react";
-//todo配列の型定義
-import type { Todo } from "./types/types";
+import { useTodos } from "./hooks/useTodos"; //自作フックを読み込む
 
 export const App = () => {
-  // ===A. 状態（state）の定義===
-  // ---データの保存（localStorage)---
-  // 1. 初期値の読み込み
-  // useStateの初期値に関数を渡すと、初回起動時に一度だけLocalStorageを見に行きます
-  const [todos, setTodos] = useState<Todo[]>(() => {
-    const saveTodos = localStorage.getItem("todo-app-data");
-    //ローカルストレージにデータが入っていれば配列データとして取り出して、なければ空の配列を返す
-    return saveTodos ? JSON.parse(saveTodos) : [];
-  });
-  // 2. 自動保存
-  // todosの中身がわかるたびに、LocalStorageに中身を書き込む
-  useEffect(() => {
-    localStorage.setItem("todo-app-data", JSON.stringify(todos));
-  }, [todos]);
-  const [inputValue, setInputValue] = useState("");
-
-  // === B/ロジック（関数）===
-  //文字が入力された時の処理の関数
-  const handleAddTodo = () => {
-    //空文字や空白はガードする（早期リターン）
-    if (!inputValue.trim()) return;
-    const newTodo: Todo = {
-      id: crypto.randomUUID(),
-      text: inputValue,
-      completed: false,
-    };
-    //入力されたら（状態が変わったら）todosに追加する
-    setTodos([...todos, newTodo]);
-    //入力欄に残っているテキストを削除する
-    setInputValue("");
-  };
-  //完了フラグの切り替えの関数(チェックボックスの完了、未完了の切り替え)
-  const toggleTodo = (id: string) => {
-    setTodos(
-      todos.map((t) => (t.id === id ? { ...t, completed: !t.completed } : t)),
-    );
-  };
-
-  //deleteボタンが押された時の処理関数
-  const deleteTodo = (id: string) => {
-    setTodos(todos.filter((t) => t.id !== id));
-  };
+  //useTodosから必要なものだけを取り出す（分割代入）
+  const {
+    todos,
+    inputValue,
+    setInputValue,
+    handleAddTodo,
+    toggleTodo,
+    deleteTodo,
+  } = useTodos();
 
   // ===UI（JSX)===
   return (
@@ -56,14 +21,15 @@ export const App = () => {
           onSubmit={(e) => {
             e.preventDefault(); //ページのリロードを防ぐ（必須）
             handleAddTodo(); //これまでの追加ロジックを実行
-          }}>
+          }}
+        >
           <input
             type="text"
             value={inputValue}
             onChange={(e) => setInputValue(e.target.value)}
             placeholder="入力してください"
           />
-          <button onClick={handleAddTodo}>追加</button>
+          <button>追加</button>
         </form>
       </div>
 
---
+++ src/hooks/useTodos.ts
@@ -0,0 +1,60 @@
+import { useState, useEffect } from "react";
+//todo配列の型定義
+import type { Todo } from "../types/types";
+
+export const useTodos = () => {
+  // ===A. 状態（state）の定義===
+  // ---データの保存（localStorage)---
+  // 1. 初期値の読み込み
+  // useStateの初期値に関数を渡すと、初回起動時に一度だけLocalStorageを見に行きます
+  const [todos, setTodos] = useState<Todo[]>(() => {
+    const saveTodos = localStorage.getItem("todo-app-data");
+    //ローカルストレージにデータが入っていれば配列データとして取り出して、なければ空の配列を返す
+    return saveTodos ? JSON.parse(saveTodos) : [];
+  });
+
+  const [inputValue, setInputValue] = useState("");
+
+  // 2. 自動保存
+  // todosの中身がわかるたびに、LocalStorageに中身を書き込む
+  useEffect(() => {
+    localStorage.setItem("todo-app-data", JSON.stringify(todos));
+  }, [todos]);
+  
+  // === B/ロジック（関数）===
+  //文字が入力された時の処理の関数
+  const handleAddTodo = () => {
+    //空文字や空白はガードする（早期リターン）
+    if (!inputValue.trim()) return;
+    const newTodo: Todo = {
+      id: crypto.randomUUID(),
+      text: inputValue,
+      completed: false,
+    };
+    //入力されたら（状態が変わったら）todosに追加する
+    setTodos([...todos, newTodo]);
+    //入力欄に残っているテキストを削除する
+    setInputValue("");
+  };
+  //完了フラグの切り替えの関数(チェックボックスの完了、未完了の切り替え)
+  const toggleTodo = (id: string) => {
+    setTodos(
+      todos.map((t) => (t.id === id ? { ...t, completed: !t.completed } : t)),
+    );
+  };
+
+  //deleteボタンが押された時の処理関数
+  const deleteTodo = (id: string) => {
+    setTodos(todos.filter((t) => t.id !== id));
+  };
+
+  //最後に「App.tsx」側で使いたいものだけを公開する
+  return {
+    todos,
+    inputValue,
+    setInputValue,
+    handleAddTodo,
+    toggleTodo,
+    deleteTodo
+  }
+};
